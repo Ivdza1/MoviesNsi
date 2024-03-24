8e1751d229217c576f36cd37be1d336930bf15f5
Ivdza1 <ivanpopovic634@gmail.com>
2024-03-24 20:52:42 +0100

    Added dbcontext to scheme handler

--- MoviesNsi/MoviesNsi.Api/Auth/Schemes/HeaderBasicAuthSchemeHandler.cs
+++ MoviesNsi/MoviesNsi.Api/Auth/Schemes/HeaderBasicAuthSchemeHandler.cs
@@ -1,20 +1,25 @@
 using System.Security.Claims;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using MoviesNsi.Application.Common.Interfaces;
 using MoviesNsi.Auth.Options;
 
 namespace MoviesNsi.Auth.Schemes;
 
 public class HeaderBasicAuthSchemeHandler : AuthenticationHandler<HeaderBasicAuthSchemeOptions>
 {
+    private readonly IMoviesNsiDbContext _dbContext;
     [Obsolete("Obsolete")]
-    public HeaderBasicAuthSchemeHandler(IOptionsMonitor<HeaderBasicAuthSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+    public HeaderBasicAuthSchemeHandler(IOptionsMonitor<HeaderBasicAuthSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IMoviesNsiDbContext dbContext) : base(options, logger, encoder, clock)
     {
+        _dbContext = dbContext;
     }
 
-    public HeaderBasicAuthSchemeHandler(IOptionsMonitor<HeaderBasicAuthSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
+    public HeaderBasicAuthSchemeHandler(IOptionsMonitor<HeaderBasicAuthSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, IMoviesNsiDbContext dbContext) : base(options, logger, encoder)
     {
+        _dbContext = dbContext;
     }
 
     protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
@@ -26,6 +31,9 @@ public class HeaderBasicAuthSchemeHandler : AuthenticationHandler<HeaderBasicAut
             
             var password = Request.Headers[Options.PasswordHeader].FirstOrDefault() ??
                               throw new InvalidOperationException("Missing password header");
+
+            var movies = await _dbContext.Movies.ToListAsync();
+            
             var user = Options.Users.SingleOrDefault(user =>
                            user.Username.Equals(username, StringComparison.OrdinalIgnoreCase) &&
                            user.Password.Equals(password, StringComparison.OrdinalIgnoreCase)) ??
